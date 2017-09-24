d202fd5472954414f946409f245808d9c9ee4a1c
Apoorv Vardhan <siddharthlanka@gmail.com>
2017-09-24 21:49:34 +0530

    Added mlab setup and deployed to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://nomad1072:qwerty@ds147964.mlab.com:47964/todo-api' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var {User} = require('./models/user');
 
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -74,8 +75,8 @@ app.get('/todos/:id', (req, res) => {
 
 module.exports = {app};
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 
