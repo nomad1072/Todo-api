fba1e6be07ae51aab4d989ee855aeb92e9137a80
Apoorv Vardhan <siddharthlanka@gmail.com>
2017-09-26 23:03:53 +0530

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,87 @@
 var mongoose = require('mongoose');
+var validator = require('validator');
+var jwt = require('jsonwebtoken');
+var _ = require('lodash');
+// {
+//     email: 'andrew@example.com',
+//     password: 'myPass123',
+//     tokens: [{
+//         access: 'auth',
+//         token: 'long encrypted string'
+//     }]
+// }
 
-var User = mongoose.model('Users', {
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+// Instance methods are called with methods, model methods use statics
+
+// Instance method get called with individual document for this binding, model methods are called with model with this binding
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc1234');
+    } catch (e) {
+        return new Promise((resolve, reject) => {
+            reject();
+        });
+        // return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+}
+
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc1234').toString();
+
+    user.tokens.push({access, token});
+
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {
     User
